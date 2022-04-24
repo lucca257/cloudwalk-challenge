553e1a710811542bfbbb3a73e9256996e5c1e47a
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-04-24 18:26:48 -0300

    test: should discount player kills when die by world

--- src/test/unit/report.test.js
+++ src/test/unit/report.test.js
@@ -1,7 +1,10 @@
 const Report = require('../../services/ReportService');
-const ReportService = new Report();
+var ReportService = new Report();
 
 describe('unit report game test', () => {
+    afterEach(() => {
+        ReportService = new Report();
+    });
 
     test('should recognize a player nickname from log', () => {
         const mock_game_log_line = " 20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0"
@@ -64,8 +67,28 @@ describe('unit report game test', () => {
         const match_players_kills = ReportService.matches[0].kills
         expect(match_players_kills).toStrictEqual(expected_result)
     });
-    //
-    // test('should discount player kills when die by world', () => {});
+
+    test('should discount player kills when die by world', () => {
+        const mock_match_log_data = [
+            "22:17 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH",
+            "22:18 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH",
+            "1:32 Kill: 1022 4 22: <world> killed Isgalamido by MOD_TRIGGER_HURT",
+            "22:17 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH",
+            "22:18 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH",
+            "1:32 Kill: 1022 4 22: <world> killed Isgalamido by MOD_TRIGGER_HURT",
+        ]
+        const expected_result = [{
+                player: 'Isgalamido',
+                kills: 2
+            }]
+        ReportService._initMatch(1)
+        mock_match_log_data.forEach(line => {
+            const kill_information = ReportService._getKillInformation(line)
+            ReportService.registerKill(1, kill_information.killer, kill_information.killed)
+        })
+        const match_players_kills = ReportService.matches[0].kills
+        expect(match_players_kills).toStrictEqual(expected_result)
+    });
     //
     // test('should count total players kills', () => {});
     //

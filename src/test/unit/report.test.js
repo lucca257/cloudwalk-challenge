0291589b16fd0b6fca8c7058a22402f549c3750e
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-04-24 15:51:01 -0300

    test: should return kill information from log

--- src/test/unit/report.test.js
+++ src/test/unit/report.test.js
@@ -17,10 +17,20 @@ describe('unit report game test', () => {
         ]
     });
 
-    test('should recognize a player nickname', () => {
+    test('should recognize a player nickname from log', () => {
         const mock_game_log_line = " 20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0"
         const player_nickname = ReportService._getPlayerNickname(mock_game_log_line)
         expect(player_nickname).toBe('Isgalamido');
+    });
+
+    test('should return kill information from log', () => {
+        const mock_game_log_line = "22:18 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH"
+        const kill_information = ReportService._getKillInformation(mock_game_log_line)
+        expect(kill_information).toStrictEqual({
+            killer: 'Isgalamido',
+            killed: 'Isgalamido',
+            death_cause: 'MOD_ROCKET_SPLASH'
+        });
     });
 
     // test('should register player on match list', () => {});
